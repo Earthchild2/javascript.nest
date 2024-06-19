79b3f88023b5538824d037c9c899ac7dd9e19b3e
Earthchild2 <nthabiseng.fakude2@gmail.com>
2024-06-19 12:35:05 +0200

    added second work

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-let friends =[["mpho","pales","lerato","faith"]
-               ["butter","flour","milk","coconut"]
-               ["spoon","cup","jar","woodenspoon"]
-               ["vanilla","water","icing","gloves"]
-            ]
+// let friends =[["mpho","pales","lerato","faith"]
+//                ["butter","flour","milk","coconut"]
+//                ["spoon","cup","jar","woodenspoon"]
+//                ["vanilla","water","icing","gloves"]
+//             ]
+
+
+[{name:"owami",surname:"mhlongo",age:"18",isfemale},
+  {name:"sumsang",serialnumber:"25463766",year:"2020",module:"galaxy"},
+  {name:"grapes",sugerlevel:"lesssuger",callories:"10",season:"summer"}
+]
+
+
 
 
 
