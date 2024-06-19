eb770b74128b9835a9f2c90d8da3caae431341a3
Earthchild2 <nthabiseng.fakude2@gmail.com>
2024-06-19 12:06:14 +0200

    added first work

---
+++ main.js
@@ -0,0 +1,12 @@
+let friends =[["mpho","pales","lerato","faith"]
+               ["butter","flour","milk","coconut"]
+               ["spoon","cup","jar","woodenspoon"]
+               ["vanilla","water","icing","gloves"]
+            ]
+
+
+
+
+  
+
+             
